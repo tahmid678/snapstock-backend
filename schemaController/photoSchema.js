5a9fee176202a5ab2783c31a44496e9d6aa319b6
Tahmid Jawad An-Noor <tjawad98@gmail.com>
2024-04-07 05:53:55 +0600

    added comments to the schema files

--- schemaController/photoSchema.js
+++ schemaController/photoSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+// schema for photo
 const photoSchema = new Schema({
     name: {
         type: String,
--- schemaController/userSchema.js
+++ schemaController/userSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+// schema for user
 const userSchema = new Schema({
     firstName: {
         type: String,
